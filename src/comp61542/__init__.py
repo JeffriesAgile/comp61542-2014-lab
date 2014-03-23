2eea430f2ca369fd6e9176f671112d92e8ec3997
CipherHat <cipherhat@gmail.com>
2014-03-23 08:50:41 +0000

    REFACTOR

--- src/comp61542/__init__.py
+++ src/comp61542/__init__.py
@@ -2,13 +2,9 @@ from flask import Flask
 from forms import contact
 
 app = Flask(__name__, static_url_path = "", static_folder = "static")
-app.secret_key = 'jeffries key'
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'jeffries.uom@gmail.com'
-app.config["MAIL_PASSWORD"] = 'JeffriesCool'
+app.config.from_object('config')
 
+print "haha", app.static_folder
 contact.initialise(app)
 
 from comp61542 import views
---
+++ src/config.py
@@ -0,0 +1,9 @@
+__author__ = 'CipherHat'
+
+CSRF_ENABLED = True
+SECRET_KEY = 'jeffries key'
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'jeffries.uom@gmail.com'
+MAIL_PASSWORD = 'JeffriesCool'
