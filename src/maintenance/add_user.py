cea9a257888ee2cf7da34737386fec02114bc975
dumbastic <dumbastic@gmail.com>
2014-03-28 16:48:59 +0000

    new app.db

--- src/maintenance/add_user.py
+++ src/maintenance/add_user.py
@@ -3,6 +3,6 @@ __author__ = 'CipherHat'
 from comp61542 import auth_db
 from comp61542.database import models
 
-u = models.User(username='john', password='haha', email='john@email.com', role=models.ROLE_USER)
+u = models.User(username='admin', password='admin', email='admin@email.com', role=models.ROLE_ADMIN)
 auth_db.session.add(u)
 auth_db.session.commit()
