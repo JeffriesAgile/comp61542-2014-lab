fda5712ddb83365abf04c5bb4740263694f8a59d
dumbastic <dumbastic@gmail.com>
2014-04-04 23:52:33 +0700

    change sorting algorithm based on "first name + last name", instead of "full name". For instance, Jay Chin should appear before Jay (Subbarao) Kola

--- test/test_database.py
+++ test/test_database.py
@@ -290,25 +290,25 @@ class TestDatabase(unittest.TestCase):
         # Expected return: [full name, last name, first name]
         # No middlename
         data = db.split_author_name("Samuel Alexander")
-        self.assertEqual(len(data), 3, "There's a mismatch between the table length and the expected name split")
+        self.assertEqual(len(data), 4, "There's a mismatch between the table length and the expected name split")
         self.assertEqual(data[0], "Samuel Alexander", "The full name is wrong")
         self.assertEqual(data[1], "Alexander", "The last name is wrong")
         self.assertEqual(data[2], "Samuel", "The first name is wrong")
         # One middlename
         data = db.split_author_name("Samuel Alexander Checkov")
-        self.assertEqual(len(data), 3, "There's a mismatch between the table length and the expected name split")
+        self.assertEqual(len(data), 4, "There's a mismatch between the table length and the expected name split")
         self.assertEqual(data[0], "Samuel Alexander Checkov", "The full name is wrong")
         self.assertEqual(data[1], "Checkov", "The last name is wrong")
         self.assertEqual(data[2], "Samuel", "The first name is wrong")
         # Two middlenames
         data = db.split_author_name("Leonard Alexander R. Qwerty")
-        self.assertEqual(len(data), 3, "There's a mismatch between the table length and the expected name split")
+        self.assertEqual(len(data), 4, "There's a mismatch between the table length and the expected name split")
         self.assertEqual(data[0], "Leonard Alexander R. Qwerty", "The full name is wrong")
         self.assertEqual(data[1], "Qwerty", "The last name is wrong")
         self.assertEqual(data[2], "Leonard", "The first name is wrong")
         # Special case: only one name
         data = db.split_author_name("Uli")
-        self.assertEqual(len(data), 3, "There's a mismatch between the table length and the expected name split")
+        self.assertEqual(len(data), 4, "There's a mismatch between the table length and the expected name split")
         self.assertEqual(data[0], "Uli", "The name is wrong")
         self.assertEqual(data[1], "Uli", "The name is wrong")
         self.assertEqual(data[2], "", "The special case is not well handled")
